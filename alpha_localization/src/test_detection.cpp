f9ec7a6e72ca988da0c08c213263cd6562e27787
Ken <kenkiki94305@gmail.com>
2016-07-11 13:04:12 +0900

    improved marker detection and added PnP

--- alpha_localization/src/test_detection.cpp
+++ alpha_localization/src/test_detection.cpp
@@ -16,13 +16,16 @@ float calc_dist(Point2f &p1, Point2f &p2){
   return sqrt(pow(p1.x-p2.x,2)+pow(p1.y-p2.y,2));
 
 }
+
 void process(Mat &input, Mat &output){
   double start = get_dtime();
   Mat gray;
   cvtColor(input,gray,CV_BGR2GRAY);
-  //  adaptiveThreshold(gray, output, 255, ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY,9,12);
-  Mat thresh_img;
+  Mat thresh_img,thresh_img2;
+  //  adaptiveThreshold(gray,thresh_img, 255, ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY,9,10);
+  //  imshow("image1",thresh_img);
   threshold(gray, thresh_img, 100, 255, THRESH_BINARY);
+  imshow("image2",thresh_img);
   //  std::vector<std::vector<Point2f> > contours;
   output = Mat::zeros(thresh_img.rows, thresh_img.cols, CV_8UC3);
   std::vector<std::vector<Point> > contours,filtered;
@@ -46,39 +49,81 @@ void process(Mat &input, Mat &output){
     double area_raw = contourArea(contours[i]);
     area.push_back(area_rect);
     area_2.push_back(area_raw);
-    if(area_rect > area_raw*1.3)
+    if(area_rect > area_raw*1.5)
       hierarchy[i][2] = -1;
   }
-
+  vector<int> inlier_idx;
   for(int i = 0; i < contours.size(); i++){
     if(hierarchy[i][2] == -1)
       continue;
     
     int j = hierarchy[i][2];
     while(j!=-1){
-      if(area[j] < area[i] *0.6 && area[j] > area[i]*0.4){
-	filtered.push_back(contours[i]);
-	filtered.push_back(contours[j]);
+      if(area[j] < area[i] *0.9 && area[j] > area[i]*0.1 && area[j] > 25 ){
+	Moments m_i = moments(contours[i]);
+	Moments m_j = moments(contours[j]);
+	if(pow(m_i.m10/m_i.m00 - m_j.m10/m_j.m00, 2)+
+	   pow(m_i.m01/m_i.m00 - m_j.m01/m_j.m00, 2) < 100)
+	  inlier_idx.push_back(i);
+	//	inlier_idx.push_back(j);
 	//	break;
       }
       j = hierarchy[j][1];
     }
-
   }
-
+  double max_area = 0;
+  double max_idx = -1;
+  for(int i = 0; i < inlier_idx.size(); i++){
+    if(area_2[inlier_idx[i]] > max_area){
+      max_area = area_2[inlier_idx[i]];
+      max_idx = inlier_idx[i];
+    }
+  }
+  if(max_idx!=-1){
+    filtered.push_back(contours[max_idx]);
+    filtered.push_back(contours[hierarchy[max_idx][2]]);
+  }
   contours = filtered;
-  
-  /*  for( ; idx >= 0; idx = hierarchy[idx][0] )
-    {
-      Scalar color( rand()&255, rand()&255, rand()&255 );
-      drawContours( output, contours, idx, color, CV_FILLED, 8, hierarchy );
-      }*/
-  //  findContours(thresh_img, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-  //  thresh_img.copyTo(output);
-  
-  drawContours(output, contours, -1, Scalar(50,50,50));
+  vector<Point2f> corners;
+  for(int i = 0; i < contours.size(); i++){
+    RotatedRect r = minAreaRect(contours[i]);
+    Point2f pts[4];
+    r.points(pts);
+    for(int j = 0; j < 4; j++){
+      corners.push_back(pts[j]);
+    }
+  }
+  if(corners.size() > 0){
+    cornerSubPix(gray, corners, Size(2,2), 
+		 Size(-1, -1), 
+		 TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 30, 0.1));
+    for(int i = 0; i < corners.size(); i++){
+      std::cout<<corners[i]<<std::endl;
+      circle(output, corners[i], 5, Scalar(0,200,100), 3, 4);
+    }
 
-  std::cout<<"computation time "<<get_dtime()-start<<std::endl;
+  vector<Point3f> objectPoints;
+  objectPoints.push_back(Point3f(-1.85,7.9,0));
+  objectPoints.push_back(Point3f(-1.85,-2.3,0));
+  objectPoints.push_back(Point3f(7.3,-2.3,0));  
+  objectPoints.push_back(Point3f(7.3,7.9,0));
+
+  objectPoints.push_back(Point3f(0,5.5,0));
+  objectPoints.push_back(Point3f(0,0,0));
+  objectPoints.push_back(Point3f(5.4,0,0));  
+  objectPoints.push_back(Point3f(5.4,5.5,0));
+  Mat distCoeffs = (Mat_<float>(1,5) << 0.182276,-0.533582,0.000520,-0.001682,0.000000);
+  Mat camMatrix  = (Mat_<float>(3,3) << 
+		    743.023418,0.000000,329.117496,
+		    0.000000,745.126083,235.748102,
+		    0.000000,0.000000,1.000000);
+
+  Mat rvec,tvec;
+  solvePnP(objectPoints, corners, camMatrix, distCoeffs, rvec, tvec);
+ //  std::cout<<rvec<<std::endl;
+  std::cout<<tvec<<std::endl;
+  }
+  //  std::cout<<"computation time "<<get_dtime()-start<<std::endl;
 }
 
 
@@ -86,6 +131,10 @@ int main(int argc, char *argv[]){
   ros::init(argc, argv, "chessboard_test");
   ros::NodeHandle nh;
   namedWindow("detection_result");
+  startWindowThread();
+  namedWindow("image1");
+  namedWindow("image2");
+  namedWindow("input");
   startWindowThread();
 
   VideoCapture input;
@@ -95,6 +144,7 @@ int main(int argc, char *argv[]){
     input.retrieve(image);
     process(image,result);
     imshow("detection_result",result);
+    imshow("input",image);
   }
   return 0;
 }
