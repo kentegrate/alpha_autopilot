9f7544e3a9ab868f0231a67b387a6bc983b706a5
Ken Takaki <kenkiki94305@gmail.com>
2016-08-08 23:40:46 +0000

    enabled rcin

--- alpha_autopilot/src/AutoPilot_node.cpp
+++ alpha_autopilot/src/AutoPilot_node.cpp
@@ -4,18 +4,18 @@
 #include <alpha_drivers/decoder/RCInput_RPI.h>
 #include <alpha_msgs/RC.h>
 
-//RCInput_RPI rcin;
+RCInput_RPI rcin;
 std::vector<int> pulse(LINUX_RC_INPUT_NUM_CHANNELS,0);
 
 ros::Publisher rc_pub;
 void termination_handler(int sig){
-  //  rcin.deinit();
-  //  rcin.running = false;
+  rcin.deinit();
+  rcin.running = false;
   ros::shutdown();
 }
 
 void rcInEvent(const ros::TimerEvent &msg, AutoPilot *autopilot){
-  /*  rcin._timer_tick();
+  rcin._timer_tick();
   if(rcin.new_input()){
     alpha_msgs::RC msg;
     for(int i = 0; i < LINUX_RC_INPUT_NUM_CHANNELS; i++){
@@ -24,7 +24,7 @@ void rcInEvent(const ros::TimerEvent &msg, AutoPilot *autopilot){
     }
     rc_pub.publish(msg);
     autopilot->setRCIn(pulse);
-    }*/
+  }
 }
 void autopilotEvent(const ros::TimerEvent &msg, AutoPilot *autopilot){
   autopilot->update();
@@ -38,26 +38,26 @@ int main(int argc, char* argv[]){
   AutoPilot autopilot;
   autopilot.init();
   
-  //  GPIO_RPI gpio;
-  //  rcin.set_gpio(&gpio);
+  GPIO_RPI gpio;
+  rcin.set_gpio(&gpio);
   for(int i = 0; i < 64; i++){
     struct sigaction sa;
     memset(&sa,0,sizeof(sa));
     sa.sa_handler = termination_handler;
     sigaction(i,&sa,NULL);
   }
-  //  gpio.init();
-  //  rcin.init();
+  gpio.init();
+  rcin.init();
 
-  //  ros::Timer rcin_timer = nh.createTimer(ros::Duration(0.002),boost::bind(rcInEvent,_1,&autopilot));
+  ros::Timer rcin_timer = nh.createTimer(ros::Duration(0.002),boost::bind(rcInEvent,_1,&autopilot));
   ros::Timer autopilot_timer = nh.createTimer(ros::Duration(0.01),boost::bind(autopilotEvent,_1,&autopilot));
 
   ros::Rate rate(500);
-  //  while(rcin.ok()){
-  while(ros::ok()){
+  while(rcin.ok()){
+
     ros::spinOnce();
     rate.sleep();
-    //  }
+
   }
 
   return 0;
