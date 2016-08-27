d7aca9d5b48710f301c9423d0735bdd2330d834b
Ken Takaki <kenkiki94305@gmail.com>
2016-08-27 23:41:59 +0000

    tuned landing and rise

--- alpha_autopilot/src/Missions.cpp
+++ alpha_autopilot/src/Missions.cpp
@@ -107,7 +107,7 @@ AlphaState RiseTurn::get_setpoint(AlphaState state){
  }
  else{
    pid_throttle = true;
-   setpoint.pos.z = initial_state.pos.z+10;
+   setpoint.pos.z = initial_state.pos.z+6;
  }
  
  pid_reset = false;
